0bc3c3b388371569c63fe81b1a3daaceb5553dde
HixRaid <qqwweerrttsava@gmail.com>
2023-12-19 23:03:17 +0300

    Added test parse format

---
+++ pkg/utils/parse_format_test.go
@@ -0,0 +1,27 @@
+package utils
+
+import (
+	"testing"
+
+	"github.com/hixraid/dummy-image/pkg/data"
+)
+
+func TestParseFormat(t *testing.T) {
+	formats := map[string]struct {
+		Format data.ImageFormat
+		Exist  bool
+	}{
+		"png":  {data.PNG, true},
+		"jpeg": {data.JPEG, true},
+		"svg":  {data.SVG, true},
+		"abc":  {"", false},
+		"def":  {"", false},
+	}
+
+	for k, v := range formats {
+		format, exist := ParseFormat(k)
+		if format != v.Format || exist != v.Exist {
+			t.Errorf("incorrect result for '%s'", k)
+		}
+	}
+}
